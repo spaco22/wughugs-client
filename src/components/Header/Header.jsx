03dd0b149cfb396f269ce5d626133dc2713539c9
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-25 11:15:02 -0800

    create simple nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Header.scss";
+import { NavLink, Link } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <header>
+      <h1>WugHugs</h1>
+
+
+        <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+
+        </nav>
+
+    </header>
   );
 }
 
