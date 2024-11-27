fde76d3d65fb2bef97aec3fe6c1d6813414b3028
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 10:59:31 -0800

    add styling to about us page

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -6,16 +6,31 @@ function AboutPage() {
     <main className="about">
       <h1 className="about__title">Learn a little about us!</h1>
 
-      <h2 className="about__subtitle">Mission:</h2>
-      <p className="about__text">Connect wug friends with more friends!</p>
-      <h2 className="about__subtitle">Vision:</h2>
-      <p className="about__text">Create a platform to share resources, knowledge, and regulations for creating a wug home.</p>
-      <h2 className="about__subtitle">Values:</h2>
-      <ul className="about__values">
-        <li className="value">No question is too silly to ask!</li>
-        <li className="value">Be respectful</li>
-        <li className="value">Have fun!</li>
-      </ul>
+      <section className="statements">
+        <article className="statement">
+          <h2 className="statement__title">Mission:</h2>
+          <p className="statement__text">
+            Connect wug friends with more friends
+          </p>
+        </article>
+
+        <article className="statement">
+          <h2 className="statement__title">Vision:</h2>
+          <p className="statement__text">
+            Create a platform to share resources, knowledge, and regulations for
+            creating a wug home
+          </p>
+        </article>
+
+        <article className="statement">
+          <h2 className="statement__title">Values:</h2>
+          <ul className="statement__values">
+            <li className="statement__value">No question is too silly to ask</li>
+            <li className="statement__value">Be respectful</li>
+            <li className="statement__value">Have fun!</li>
+          </ul>
+        </article>
+      </section>
     </main>
   );
 }
