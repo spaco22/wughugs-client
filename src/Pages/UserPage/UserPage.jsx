879841e49f40f061e29f5c3707e943022c0ee9b0
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 14:13:22 -0800

    add styling to user page

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -3,33 +3,40 @@ import "./UserPage.scss";
 
 function UserPage() {
   return (
-    <main className="user">
-        <h1 className="user__title">Welcome back!</h1>
+    <main className="user-page">
+        <h1 className="user-page__title">Welcome back!</h1>
 
+        <section className="user">
         <div className="user__img"></div>
 
-        <h2 className="user__name">Sofia</h2>
-        <h4 className="user__location">Hornby Island</h4>
+        <article className="user__text">
+        <h3 className="user__name">Sofia</h3>
+        <p className="user__location">Hornby Island</p>
+        </article>
+        </section>
 
-        <section className="wugs">
+        <div className="wugs">
+        <h2 className="wugs__title">Wugs</h2>
+        <section className="wugs__gallery">
         <article className="wug">
             <div className="wug__img"></div>
-            <h3 className="wug__name">Charlotte</h3>
+            <h4 className="wug__name">Charlotte</h4>
             <p className="wug__species">pider</p>
         </article>
 
         <article className="wug">
             <div className="wug__img"></div>
-            <h3 className="wug__name">Charlie</h3>
+            <h4 className="wug__name">Charlie</h4>
             <p className="wug__species">pider</p>
         </article>
 
         <article className="wug">
             <div className="wug__img"></div>
-            <h3 className="wug__name">Mealwormies</h3>
+            <h4 className="wug__name">Mealwormies</h4>
             <p className="wug__species">meal worms</p>
         </article>
         </section>
+        </div>
 
         <button className="user__button-wug">Add Wug</button>
     </main>
