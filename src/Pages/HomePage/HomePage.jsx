0b36464f687f81022c0b926143943148ff171739
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 08:56:47 -0800

    create homepage skeleton

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,9 +3,21 @@ import "./HomePage.scss";
 
 function HomePage() {
   return (
-    <>
+    <main className="homepage">
       <h1>HomePage</h1>
-    </>
+
+      <section className="card">
+        <h2 className="card__title">Make Friends</h2>
+      </section>
+
+      <section className="card">
+        <h2 className="card__title">Share Knowledge</h2>
+      </section>
+
+      <section className="card">
+        <h2 className="card__title">Prepare to Experiment</h2>
+      </section>
+    </main>
   );
 }
 
