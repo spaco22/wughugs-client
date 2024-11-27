adf8892f6096877c7f81f45cca79593bc4e08935
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 07:59:46 -0800

    add baseline styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,14 @@ import { NavLink, Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <h1 className="header__title">Wug Hugs</h1>
 
 
         <nav className="header__nav">
         <NavLink to="/" className="header__link">Home</NavLink>
         <NavLink to="/about" className="header__link">About</NavLink>
+        <NavLink to="/" className="header__link">Login</NavLink>
 
         </nav>
 
