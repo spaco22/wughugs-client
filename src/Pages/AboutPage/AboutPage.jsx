8e5ef7551ea6e43cd39a79ed3e5c35ab64ac42d5
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 10:37:31 -0800

    create outline for about page, add li styling

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -3,9 +3,20 @@ import "./AboutPage.scss";
 
 function AboutPage() {
   return (
-    <>
-      <h1>About Us</h1>
-    </>
+    <main className="about">
+      <h1 className="about__title">Learn a little about us!</h1>
+
+      <h2 className="about__subtitle">Mission:</h2>
+      <p className="about__text">Connect wug friends with more friends!</p>
+      <h2 className="about__subtitle">Vision:</h2>
+      <p className="about__text">Create a platform to share resources, knowledge, and regulations for creating a wug home.</p>
+      <h2 className="about__subtitle">Values:</h2>
+      <ul className="about__values">
+        <li className="value">No question is too silly to ask!</li>
+        <li className="value">Be respectful</li>
+        <li className="value">Have fun!</li>
+      </ul>
+    </main>
   );
 }
 
