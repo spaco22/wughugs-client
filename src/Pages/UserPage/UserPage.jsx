8ad72ac7ac6372ed27463aba2d234c3c359d481c
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 13:04:28 -0800

    user page backbone

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "./UserPage.scss";
 
 function UserPage() {
   return (
-    <div>UserPage</div>
+    <main className="user">
+        <h1 className="user__title">Welcome back!</h1>
+
+        <div className="user__img"></div>
+
+        <h2 className="user__name">Sofia</h2>
+        <h4 className="user__location">Hornby Island</h4>
+
+        <section className="wugs">
+        <article className="wug">
+            <div className="wug__img"></div>
+            <h3 className="wug__name">Charlotte</h3>
+            <p className="wug__species">pider</p>
+        </article>
+
+        <article className="wug">
+            <div className="wug__img"></div>
+            <h3 className="wug__name">Charlie</h3>
+            <p className="wug__species">pider</p>
+        </article>
+
+        <article className="wug">
+            <div className="wug__img"></div>
+            <h3 className="wug__name">Mealwormies</h3>
+            <p className="wug__species">meal worms</p>
+        </article>
+        </section>
+
+        <button className="user__button-wug">Add Wug</button>
+    </main>
   )
 }
 
