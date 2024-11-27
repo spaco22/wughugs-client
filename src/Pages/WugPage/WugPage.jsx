fb40d9ec73d8c933d3cf9e9758e52962dca63046
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-27 14:32:50 -0800

    add wug details section to wug page

--- src/Pages/WugPage/WugPage.jsx
+++ src/Pages/WugPage/WugPage.jsx
@@ -3,7 +3,28 @@ import "./WugPage.scss";
 
 function WugPage() {
   return (
-    <div>WugPage</div>
+    <main className="wug">
+
+      <section className="wug__details">
+      <article className="wug">
+        <div className="wug__img"></div>
+        <h3 className="wug__name">Chalotte</h3>
+        <p className="wug__species">Tarantula</p>
+        <p className="wug__age"> approx. 1 year 2 months</p>
+        <p className="wug__type">Wug</p>
+        <p className="wug__common-names">Tarantula, ...</p>
+      </article>
+
+      <article className="user">
+        <div className="user__img"></div>
+        <h4 className="user__name">Sofia</h4>
+        <p className="user__location">Hornby Island</p>
+
+      </article>
+      </section>
+      
+
+    </main>
   )
 }
 
