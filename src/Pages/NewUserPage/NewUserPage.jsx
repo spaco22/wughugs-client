7fe30242bd90e29d5163920ed5d89fca8cef10c8
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-29 12:43:24 -0800

    api req to get users

--- src/Pages/NewUserPage/NewUserPage.jsx
+++ src/Pages/NewUserPage/NewUserPage.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import "./NewUserPage.scss";
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate,useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function NewUserPage() {
 
     const nav = useNavigate();
 
+    const [users, setUsers] = useState([]);
+
+  
+    async function getUsers() {
+      try {
+        const response = await axios.get(`${baseURL}/users`);
+        // console.log(response.data);
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error retrieving users data", error);
+      }
+    }
+
     const handleLoginClick = (event) => {
         nav("/login");
     }
+
+    useEffect(() => {
+      getUsers();
+    }, []);
 
   return (
     <main className="new-user">
@@ -17,7 +36,7 @@ function NewUserPage() {
         <div className="new-user__img"></div>
 
         <form className="form" action="submit">
-            <label htmlFor="name" className="form__label">Name</label>
+            <label htmlFor="name" className="form__label">Username</label>
             <input type="text" className="form__input" name="name"/>
 
             <label htmlFor="email" className="form__label">Email</label>
