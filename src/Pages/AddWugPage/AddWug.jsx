cf3e48632284efc92917cab1d9c2896d4bf614d9
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-30 15:22:26 -0800

    add basic form styling, comment out quantity

--- src/Pages/AddWugPage/AddWug.jsx
+++ src/Pages/AddWugPage/AddWug.jsx
@@ -8,7 +8,7 @@ function AddWug() {
 
         <form action="submit" className="wug-form">
           <label htmlFor="img" className="wug-form__label">Upload Image</label>
-            <input type="image" className="wug-form__img" name="img" />
+            <input type="file" className="wug-form__img" name="img" />
 
             <label htmlFor="name" className="wug-form__label">Wug Name</label>
             <input type="text" className="wug-form__name" name="name" />
@@ -16,11 +16,11 @@ function AddWug() {
             <label htmlFor="species" className="wug-form__label">Wug Species</label>
             <input type="text" className="wug-form__species" name="species" />
 
-            <label htmlFor="quantity" className="wug-form__label">Quantity</label>
-            <input type="radio" className="wug-form__img" name="quantity" />
+            {/* <label htmlFor="quantity" className="wug-form__label">Quantity</label>
+            <input type="radio" className="wug-form__img" name="quantity" /> */}
 
-            <label htmlFor="img" className="wug-form__label">Upload Image</label>
-            <input type="image" className="wug-form__img" name="img" />
+            <label htmlFor="type" className="wug-form__label">Wug Type</label>
+            <input type="text" className="wug-form__type" name="type" />
 
             <button className="wug-form__button-add">Add Wug</button>
             <button className="wug-form__button-cancel">Cancel</button>
