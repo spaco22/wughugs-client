0c930bcc54a7ee4044df4f6c24e057208cc4acd9
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-30 14:39:12 -0800

    edit footer title to link to homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Footer.scss";
 import { NavLink } from "react-router-dom";
 import Home from "../../assets/icons/home-w.svg";
@@ -8,22 +8,23 @@ import Login from "../../assets/icons/login-w.svg";
 function Footer() {
   return (
     <footer className="footer">
+      <NavLink className="footer__title-link" to="/">
         <h2 className="footer__title">Wug Hugs</h2>
+      </NavLink>
 
-        <nav className="footer__nav">
+      <nav className="footer__nav">
         <NavLink to="/" className="footer__link">
-        <img src={Home} alt="home icon" className="footer__link-img" />
+          <img src={Home} alt="home icon" className="footer__link-img" />
         </NavLink>
         <NavLink to="/about" className="footer__link">
-        <img src={About} alt="about icon" className="footer__link-img" />
+          <img src={About} alt="about icon" className="footer__link-img" />
         </NavLink>
         <NavLink to="/login" className="footer__link">
-        <img src={Login} alt="login icon" className="footer__link-img" />
+          <img src={Login} alt="login icon" className="footer__link-img" />
         </NavLink>
-
-        </nav>
+      </nav>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
