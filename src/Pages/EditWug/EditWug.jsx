b867d33a79ee359f3e4733963607f59519c57d7f
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-30 16:39:33 -0800

    create edit wug form

--- src/Pages/EditWug/EditWug.jsx
+++ src/Pages/EditWug/EditWug.jsx
@@ -2,8 +2,49 @@ import React from 'react';
 import "./EditWug.scss";
 
 function EditWug() {
+
+    function handleCancelClick(event) {
+        confirm("Click OK to cancel");
+      }
+      
   return (
-    <div>EditWug</div>
+<main className="edit-wug">
+      <h2 className="edit-wug__title">Edit</h2>
+
+      <form action="submit" className="wug-form">
+        <label htmlFor="img" className="wug-form__label">
+          Upload Image
+        </label>
+        <input type="file" className="wug-form__img" name="img" />
+
+        <label htmlFor="name" className="wug-form__label">
+          Wug Name
+        </label>
+        <input type="text" className="wug-form__name" name="name" />
+
+        <label htmlFor="species" className="wug-form__label">
+          Wug Species
+        </label>
+        <input type="text" className="wug-form__species" name="species" />
+
+        {/* <label htmlFor="quantity" className="wug-form__label">Quantity</label>
+            <input type="radio" className="wug-form__img" name="quantity" /> */}
+
+        <label htmlFor="type" className="wug-form__label">
+          Wug Type
+        </label>
+        <input type="text" className="wug-form__type" name="type" />
+
+        <button className="wug-form__button-add">Add Wug</button>
+        <button
+          className="wug-form__button-cancel"
+          onClick={handleCancelClick}
+          type="reset"
+        >
+          Cancel
+        </button>
+      </form>
+    </main>
   )
 }
 
