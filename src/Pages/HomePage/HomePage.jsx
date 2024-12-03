ecf50fcf9b8d173200913e8d096c925402dc3ca3
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-12-03 13:52:16 -0800

    home images

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./HomePage.scss";
+import Friends from "../../assets/images/make-friends.png";
+import Knowledge from "../../assets/images/share-knowledge.png";
+import Experiment from "../../assets/images/prepare-to-experiment.png";
 
 function HomePage() {
   return (
@@ -9,14 +12,18 @@ function HomePage() {
       <div className="cards">
       <section className="card">
         <h2 className="card__title">Make Friends</h2>
+        {/* <div className="card__img"></div> */}
+        <img src={ Friends } alt="" className="card__img-f" />
       </section>
 
-      <section className="card">
+      <section className="card card--k">
         <h2 className="card__title">Share Knowledge</h2>
+        <img src={ Knowledge } alt="" className="card__img-k" />
       </section>
 
       <section className="card">
         <h2 className="card__title">Prepare to Experiment</h2>
+        <img src={ Experiment } alt="" className="card__img-e" />
       </section>
       </div>
     </main>
